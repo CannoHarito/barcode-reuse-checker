2f23bfd1f12fc84590e62a1ce24f6fcc11b932c7
CannoHarito <45878603+CannoHarito@users.noreply.github.com>
2019-04-29 12:11:50 +0900

    Add 検索結果にURL追加

--- src/client/components/item.jsx
+++ src/client/components/item.jsx
@@ -2,11 +2,14 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Panel, Row, Col } from 'muicss/react';
 
-const itemPiece = (service, price, title) =>
+const itemPiece = (service, price, title, url) =>
   (<Row key={service + title} className="mui--divider-bottom" >
     <Col xs="6" md="3" >{service}</Col>
     <Col xs="6" md="3" >{price}</Col>
-    {title && <Col xs-offset="1" md-offset="0" xs="11" md="6" >{title}</Col>}
+    {title && <Col xs-offset="1" md-offset="0" xs="11" md="6" >{
+      url && <a href={url} target="_blank" rel="noopener noreferrer">{title}</a>
+      || title
+    }</Col>}
   </Row>);
 
 const Item = (props) => {
@@ -15,7 +18,7 @@ const Item = (props) => {
     if (items.length === 0) list.push(itemPiece(service, 'No match.'));
     else {
       items.forEach((i) =>
-        list.push(itemPiece(service, i.price + '円', i.title)));
+        list.push(itemPiece(service, i.price + '円', i.title, i.url)));
     }
   });
 
--- src/server/server-utilities.js
+++ src/server/server-utilities.js
@@ -50,6 +50,9 @@ class Service {
     while ((matches = this.regexp.exec(html)) !== null) {
       items.push(this.returnFunc(matches));
     }
+    items.length >= 1 && items[0].url === undefined && items.forEach((i) => {
+      i.url = this.urlFunc(query);
+    });
     return { service: this.name, query: query, items: items };
   }
 }
@@ -60,7 +63,7 @@ const services = {
     (q) => 'https://www.bookoffonline.co.jp/boleccontent/bolbuysearch/buysearch/search?bg=&q=' + EscapeSJIS(q),
     'ms932',
     // eslint-disable-next-line max-len
-    new RegExp(/class="sell-item-title-maintitle">(.+?)<\/span>[\s\S]+?class="sell-item-price.+?>([-\d]+)円/g),
+    new RegExp(/class="sell-item-title-maintitle">(.+?)<\/span>[\s\S]+?class="sell-item-price.+?>([-\d]+)円/gm),
     (m) => ({ price: m[2], title: m[1] })
   ),
   NetOff: new Service(
@@ -68,16 +71,16 @@ const services = {
     (q) => 'https://www.netoff.co.jp/sellcont/nid/PurchaseList/cid/?P2=&P9=4&P10=2&actionNameTxt=&cboCtgry=&cboLRack=&SearchKeyWord1=' + encodeURIComponent(q) + '&hdnSearchKeyWord1=' + encodeURIComponent(q) + '&hdnSearchKeyWord2=',
     'utf-8',
     // eslint-disable-next-line max-len, no-control-regex, no-tabs
-    new RegExp(/border="">(.+?)<\/span><\/A>	<!-- タイトル -->[\s\S]+?texttype01" style="text-align:center;">([\d,]+)円<\/td>				<!-- 買取価格 -->/g),
-    (m) => ({ price: m[2], title: m[1] })
+    new RegExp(/href="([^"]+)".+?border="">(.+?)<\/span><\/A>	<!-- タイトル -->[\s\S]+?texttype01" style="text-align:center;">([\d,]+)円<\/td>				<!-- 買取価格 -->/gm),
+    (m) => ({ price: m[3], title: m[2], url: m[1] })
   ),
   Surugaya: new Service(
     '駿河屋',
     (q) => 'https://www.suruga-ya.jp/search_buy?category=&search_word=' + encodeURIComponent(q) + '&searchbox=1',
     'utf-8',
     // eslint-disable-next-line max-len, no-control-regex, no-tabs
-    new RegExp(/class="title"><a[^>]+>(.+?)<\/a>[\s\S]+?<td>([\d,]+)円<\/td>/g),
-    (m) => ({ price: m[2], title: m[1] })
+    new RegExp(/class="title"><a href="([^"]+)">(.+?)<\/a>[\s\S]+?<td>([\d,]+)円<\/td>/gm),
+    (m) => ({ price: m[3], title: m[2], url: 'https://www.suruga-ya.jp' + m[1] })
   ),
 };
 
